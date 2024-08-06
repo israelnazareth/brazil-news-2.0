5f7c65eff405eb7cbc3218717d48c1e3f8e6cbc3
Israel Nazareth <israelsnazareth@gmail.com>
2024-08-05 22:53:18 -0300

    refact: vercel.json

--- frontend/src/services/fetchApi.ts
+++ frontend/src/services/fetchApi.ts
@@ -2,7 +2,7 @@ import axios from "axios"
 
 export const fetchByCategory = async (category: string) => {
   try {
-    const response = await axios.get(`/api/${category}`)
+    const response = await axios.get(`/api/${category}`, { baseURL: 'http://localhost:5000' })
     return await response.data.articles
   } catch (error: unknown) {
     if (error instanceof Error) {
