a255c099c3fbf0ce4654382ca514b75cd9aba123
Israel Nazareth <israelsnazareth@gmail.com>
2024-08-05 21:41:43 -0300

    refact: remove localhost

--- frontend/src/services/fetchApi.ts
+++ frontend/src/services/fetchApi.ts
@@ -2,7 +2,7 @@ import axios from "axios"
 
 export const fetchByCategory = async (category: string) => {
   try {
-    const response = await axios.get(`http://localhost:5000/app/${category}`)
+    const response = await axios.get(`/app/${category}`)
     return await response.data.articles
   } catch (error: unknown) {
     if (error instanceof Error) {
